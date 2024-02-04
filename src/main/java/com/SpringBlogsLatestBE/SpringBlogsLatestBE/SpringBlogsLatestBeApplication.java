2f70fd8a399d4d2e0d4a43861548448f01ac0f58
PRASHANTPAL283 <93310128+PRASHANTPAL283@users.noreply.github.com>
2024-02-04 12:38:46 +0530

    backend code

---
+++ src/main/java/com/SpringBlogsLatestBE/SpringBlogsLatestBE/SpringBlogsLatestBeApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBlogsLatestBE.SpringBlogsLatestBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogsLatestBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogsLatestBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBlogsLatestBE/SpringBlogsLatestBE/SpringBlogsLatestBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBlogsLatestBE.SpringBlogsLatestBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogsLatestBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
