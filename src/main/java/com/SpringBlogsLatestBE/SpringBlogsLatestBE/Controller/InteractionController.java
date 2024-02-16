a369291743fc4f3407d1c07e66f3e4fe549ef053
PRASHANTPAL283 <93310128+PRASHANTPAL283@users.noreply.github.com>
2024-02-16 17:19:13 +0530

    changes again
    changes again

---
+++ src/main/java/com/SpringBlogsLatestBE/SpringBlogsLatestBE/Controller/InteractionController.java
@@ -0,0 +1,41 @@
+package com.SpringBlogsLatestBE.SpringBlogsLatestBE.Controller;
+
+import com.SpringBlogsLatestBE.SpringBlogsLatestBE.Entities.CommentModel;
+import com.SpringBlogsLatestBE.SpringBlogsLatestBE.Entities.LikeEntity;
+import com.SpringBlogsLatestBE.SpringBlogsLatestBE.Services.CommentServices;
+import com.SpringBlogsLatestBE.SpringBlogsLatestBE.Services.Likeservices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class InteractionController {
+    @Autowired
+    public Likeservices likeservices;
+
+    @Autowired
+    public CommentServices commentServices;
+    @PostMapping("/doLike")
+    public ResponseEntity<LikeEntity> doLike(@RequestBody LikeEntity likeEntity){
+        return this.likeservices.doLike(likeEntity);
+    }
+
+    @GetMapping("/getBlogs/{id}")
+    public ResponseEntity<List<LikeEntity>> getallLikes(@PathVariable("id") int id){
+        return this.likeservices.getalllikesbyblogId(id);
+    }
+
+    @PostMapping("/doComment")
+    public ResponseEntity<CommentModel> doComment(@RequestBody CommentModel commentModel){
+        return this.commentServices.addNewComment(commentModel);
+
+    }
+    @GetMapping("/allcomments/{id}")
+    public ResponseEntity<List<CommentModel>> getallComments(@PathVariable("id") int id){
+        return this.commentServices.getallcommentsByBlogId(id);
+    }
+
+
+}
