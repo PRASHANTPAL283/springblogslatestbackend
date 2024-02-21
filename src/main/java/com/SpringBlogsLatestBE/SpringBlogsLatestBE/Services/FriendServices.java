63eaebb15b31bada535c7338cd9db699eb0f6498
PRASHANTPAL283 <93310128+PRASHANTPAL283@users.noreply.github.com>
2024-02-21 12:58:30 +0530

    Update FriendServices.java

--- src/main/java/com/SpringBlogsLatestBE/SpringBlogsLatestBE/Services/FriendServices.java
+++ src/main/java/com/SpringBlogsLatestBE/SpringBlogsLatestBE/Services/FriendServices.java
@@ -2,6 +2,8 @@ package com.SpringBlogsLatestBE.SpringBlogsLatestBE.Services;
 
 import com.SpringBlogsLatestBE.SpringBlogsLatestBE.Dao.FriendsDao;
 import com.SpringBlogsLatestBE.SpringBlogsLatestBE.Entities.FriendsEntity;
+import com.SpringBlogsLatestBE.SpringBlogsLatestBE.Entities.UserModel;
+import com.SpringBlogsLatestBE.SpringBlogsLatestBE.ErrorHandler.GlobalExceptionClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,5 +34,37 @@ public class FriendServices {
 
     }
 
-    public 
+    public Boolean CheckIfFriendAlreadyPresent(int userId, int friendId){
+
+        List<FriendsEntity> list=this.getallfriends(userId);
+        Optional<UserModel> user=Optional.ofNullable(list.stream().filter(e->e.getFriendadded().getUserId()==friendId).findFirst().get().getFriendadded());
+        if(list.size()==0){
+            return false;
+        }
+        else if(user.isPresent()){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+
+    }
+
+    public ResponseEntity<FriendsEntity> addNewFriend(FriendsEntity friendsEntity){
+        if(CheckIfFriendAlreadyPresent(friendsEntity.getMyuserId(),friendsEntity.getFriendadded().getUserId())==true){
+            throw new GlobalExceptionClass("Friend already added", "500");
+        }
+        else{
+            try {
+                FriendsEntity result = this.friendsDao.save(friendsEntity);
+                return ResponseEntity.status(HttpStatus.OK).body(result);
+            }
+            catch (Exception ex){
+                throw new GlobalExceptionClass(ex.getMessage(),ex.getCause(),"500");
+            }
+        }
+    }
+
+
 }
